e97b212acbbb043a9f02049598fc9ec47ba2b8d2
moroz_alexander <morozalexander82@gmail.com>
2019-04-02 20:24:51 +0300

    created some classes and methods

---
+++ src/main/java/Auto.java
@@ -0,0 +1,48 @@
+
+public class Auto {
+    private String marka;
+    private String bodyType;
+    private double fuelСonsumption;
+    private double maxSpeed;
+    private int id;
+
+    public String getMarka() {
+        return marka;
+    }
+
+    public String getBodyType() {
+        return bodyType;
+    }
+
+    public void setMarka(String marka) {
+        this.marka = marka;
+    }
+
+    public void setBodyType(String bodyType) {
+        this.bodyType = bodyType;
+    }
+
+    public double getFuelСonsumption() {
+        return fuelСonsumption;
+    }
+
+    public void setFuelСonsumption(double fuelСonsumption) {
+        this.fuelСonsumption = fuelСonsumption;
+    }
+
+    public double getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(double maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/TaxoPark.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaxoPark extends Auto {
+   private List<Auto> autoList= new ArrayList();
+
+    public List<Auto> getAutoList() {
+        return autoList;
+    }
+
+}
